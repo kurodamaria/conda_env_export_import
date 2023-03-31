662c6ab381f6cb3accc39232dbf2ba99fc80bb28
kurodamaria <xfreunion@gmail.com>
2023-03-31 23:10:15 +0800

    correct error

--- export.py
+++ export.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 import shutil
+import json
 
 # Set the name of the output directory
 output_dir = 'environments'
@@ -11,20 +12,23 @@ if not os.path.exists(output_dir):
 
 # Get the list of all conda environments
 result = subprocess.run(['conda', 'env', 'list', '--json'], stdout=subprocess.PIPE)
-envs = result.stdout.decode('utf-8')
+envs = json.loads(result.stdout.decode('utf-8'))['envs']
 
 # Loop through each environment and export it
 for env in envs:
     # Skip the base environment
-    if env == 'base':
+    if env.endswith('anaconda3'):
         continue
 
+    # Get the name of the environment
+    env_name = os.path.basename(env)
+
     # Set the name of the output file
-    output_file = os.path.join(output_dir, f'{env}.yml')
+    output_file = os.path.join(output_dir, f'{env_name}.yml')
 
     # Run the conda env export command
-    print(f'Exporting environment: {env}')
-    subprocess.run(['conda', 'env', 'export', '-n', env, '--no-build', '-f', output_file])
+    print(f'Exporting environment: {env_name}')
+    subprocess.run(['conda', 'env', 'export', '-n', env_name, '--no-build', '-f', output_file])
 
 # Package the exported yml files
 shutil.make_archive('environments', 'zip', output_dir)
