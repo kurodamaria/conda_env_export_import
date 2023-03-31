0dd55cb22ebfa4d52750fc6357e2dc02c2a79bdc
kurodamaria <xfreunion@gmail.com>
2023-03-31 22:54:00 +0800

    no-build to support cross os; also package the exported yml files

--- export.py
+++ export.py
@@ -1,12 +1,31 @@
-import os
 import subprocess
+import os
+import shutil
 
-output = subprocess.check_output("conda env list", shell=True).decode()
-lines = output.strip().split('\n')
-for line in lines:
-    if line.startswith('#'):
+# Set the name of the output directory
+output_dir = 'environments'
+
+# Create the output directory if it doesn't exist
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# Get the list of all conda environments
+result = subprocess.run(['conda', 'env', 'list', '--json'], stdout=subprocess.PIPE)
+envs = result.stdout.decode('utf-8')
+
+# Loop through each environment and export it
+for env in envs:
+    # Skip the base environment
+    if env == 'base':
         continue
-    name = line.split()[0]
-    if name != 'base':
-        print(f"Exporting environment: {name}")
-        os.system(f"conda activate {name} && conda env export > {name}.yml")
+
+    # Set the name of the output file
+    output_file = os.path.join(output_dir, f'{env}.yml')
+
+    # Run the conda env export command
+    print(f'Exporting environment: {env}')
+    subprocess.run(['conda', 'env', 'export', '-n', env, '--no-build', '-f', output_file])
+
+# Package the exported yml files
+shutil.make_archive('environments', 'zip', output_dir)
+print(f'Packaged environments into environments.zip')
--- import.py
+++ import.py
@@ -1,7 +1,25 @@
-import glob
+import subprocess
 import os
+import shutil
 
-for file in glob.glob('*.yml'):
-    name = os.path.splitext(file)[0]
-    print(f"Creating environment: {name}")
-    os.system(f"conda env create -f {file} -n {name}")
+# Set the name of the input file
+input_file = 'environments.zip'
+
+# Unpack the input file
+shutil.unpack_archive(input_file)
+print(f'Unpacked {input_file}')
+
+# Set the name of the input directory
+input_dir = 'environments'
+
+# Loop through each file in the input directory and import it
+for file in os.listdir(input_dir):
+    # Set the name of the environment to create
+    env_name = os.path.splitext(file)[0]
+
+    # Set the name of the input file
+    input_file = os.path.join(input_dir, file)
+
+    # Run the conda env create command
+    print(f'Importing environment: {env_name}')
+    subprocess.run(['conda', 'env', 'create', '-n', env_name, '-f', input_file])
